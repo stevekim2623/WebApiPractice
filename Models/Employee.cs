aa487183dcd020d539f9fa214b5fa2b8328aa44f
Dooh Kim <kdo880126@hotmail.com>
2023-05-25 21:58:40 +0900

    Employree Class 추가

---
+++ Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApiPractice.Models
+{
+    public class Employee
+    {
+        public string? name { get; set; }
+        public string? email { get; set; }
+        public string? tel { get; set; }
+        public string? joined { get; set; }
+    }
+}
