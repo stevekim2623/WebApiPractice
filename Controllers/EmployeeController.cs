f11aacec46a65462408bbfa0e172eeaae71607ac
Dooh Kim <kdo880126@hotmail.com>
2023-05-26 11:19:12 +0900

    1. 파일 업로드 메서드 추가.  - 확장자가 없으면 예외처리.  - 확장자에 따라 읽는 방법 분기 처리. 2. ReadJson 메서드 추가.  - 데이터 길이, 잘못된 포멧으로 입력되면 예외처리. 3. EmployeeExists 메서드 추가.  - 저장해둔 Employee들과 비교하고 중복된 것들을 리턴.
    note
    ReadJson 메서드에 중복된 데이터를 저장할때 발생한 예외를 작성자가 의도한 코드로 예외처리 못함.

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -80,9 +80,105 @@ namespace WebApiPractice.Controllers
             return CreatedAtAction("GetEmployee", new { id = employee.name }, employee);
         }
 
+        // POST api/Employee/file
+        [HttpPost("file")]
+        public async Task<ActionResult> PostEmployee(IFormFile file)
+        {
+            if (!Path.HasExtension(file.FileName))
+            {
+                ActionResult actionResult = BadRequest(new { message = "This file has no file extension." });
+                return actionResult;
+            }
+
+            string extension = Path.GetExtension(file.FileName);
+            if (extension.ToLower() == ".json")
+            {
+                Stream stream = file.OpenReadStream();
+                var actionResult = await ReadJson(stream);
+                return actionResult;
+            }
+            else
+            {
+                ActionResult actionResult = BadRequest(new { message = "This file has an unsupported file extension." });
+                return actionResult;
+            }
+        }
+
         private bool EmployeeExists(string name, string joined)
         {
             return (_context.Employees?.Any(e => e.name == name && e.joined == joined)).GetValueOrDefault();
+        }
+
+        private IEnumerable<Employee> EmployeeExists(Employee[] employees)
+        {
+            List<Employee> employeeList = new List<Employee>();
+
+            foreach (var item in employees)
+            {
+                if ((_context.Employees?.Any(e => e.name == item.name && e.joined == item.joined)).GetValueOrDefault())
+                {
+                    employeeList.Add(item);
+                }
+            }
+
+            return employeeList;
+        }
+
+        private async Task<ActionResult> ReadJson(Stream stream)
+        {
+            if (stream.Length == 0)
+            {
+                ActionResult actionResult = BadRequest(new { message = "JSON file length is 0" });
+                return actionResult;
+            }
+
+            Employee[]? json;
+            try
+            {
+                var jsonOptions = new JsonSerializerOptions
+                {
+                    WriteIndented = true
+                };
+
+                json = JsonSerializer.Deserialize<Employee[]>(stream, jsonOptions);
+            }
+            catch (JsonException ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+
+            if (json == null)
+            {
+                return BadRequest(new { message = "Failed to deserialize JSON file." });
+            }
+            else
+            {
+                if (json.Length <= 0)
+                {
+                    return BadRequest(new { message = "The number of employees is confirmed as 0." });
+                }
+
+                await _context.Employees.AddRangeAsync(json);
+
+                try
+                {
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateException ex)
+                {
+                    return BadRequest(new { message = ex.Message, ActionResult = (ActionResult)EmployeeExists(json) });
+                }
+                catch (ArgumentException ex)
+                {
+                    return BadRequest(new { message = ex.Message, ActionResult = (ActionResult)EmployeeExists(json) });
+                }
+            }
+
+            return Created("", null);
         }
     }
 }
